763e865307496702835ec68f9c3165a0b7dc0b52
Carrington <Manyuchi>
2023-11-15 15:42:41 +0200

    creating header

--- Weather/Demos/WeatherHeader.swift
+++ Weather/Demos/WeatherHeader.swift
@@ -9,27 +9,65 @@ import UIKit
 
 class WeatherHeader: UITableViewHeaderFooterView {
     
+    
+    private let headerStackView: UIStackView = {
+        
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
     private let headerMinLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "25°"
+        label.text = "19°"
         label.textColor = .black
         label.font = .systemFont(ofSize: 30, weight: .semibold)
         label.numberOfLines = 1
         return label
     }()
     
-    private let minTemp: UILabel = {
+    private let headerMinTemp: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "25°"
+        label.text = "min"
         label.textColor = .black
         label.font = .systemFont(ofSize: 30, weight: .semibold)
         label.numberOfLines = 1
         return label
     }()
 
-    private let headerSummaryLabel: UILabel = {
+    private let headerCurrentTemp: UILabel = {
+        let label  = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Sunny"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 18, weight: .semibold)
+        return label
+    }()
+    
+    private let headerCurrentLabel: UILabel = {
+        let label  = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Sunny"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 18, weight: .semibold)
+        return label
+    }()
+    
+    private let headerMaxLabel: UILabel = {
+        let label  = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Sunny"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 18, weight: .semibold)
+        return label
+    }()
+    
+    private let headerMaxTemp: UILabel = {
         let label  = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Sunny"
